376a93ab677941331e27af8be8223903868a5173
liuwei <liuwei06jie@126.com>
2022-05-17 10:00:54 +0800

    add

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String helloController() {
         return "Hello !!";
+    }
+
+    @GetMapping("/user/{name}")
+    public String user(@PathVariable("name") String name) {
+        return "hello, " + name;
     }
 }
