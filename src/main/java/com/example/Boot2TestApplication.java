12573bd30f1ce09f8ee67c39abe123ea0fb96d7f
liuwei <liuwei06jie@126.com>
2022-05-17 09:17:09 +0800

    init project

---
+++ src/main/java/com/example/Boot2TestApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Boot2TestApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(Boot2TestApplication.class, args);
+	}
+
+}
